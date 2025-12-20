d5cf56143e127a1f83de0cab9e80c206e2ac3ddc
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2025-12-20 12:35:15 -0500

    chagnes commit

--- backend/src/geminiClient.js
+++ backend/src/geminiClient.js
@@ -6,10 +6,10 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "models/gemini-flash-latest"
+    model: "models/gemini-flash-latest"
 });
 
 export async function askGemini(prompt) {
-  const result = await model.generateContent(prompt);
-  return result.response.text();
+    const result = await model.generateContent(prompt);
+    return result.response.text();
 }
